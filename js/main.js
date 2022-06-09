da86ac8af405d6f354a46064e8bb768a89874895
Наталия Лифанова <tymanblack@gmail.com>
2022-06-09 12:15:52 +0300

    ESLint corrections

--- js/main.js
+++ js/main.js
@@ -4,11 +4,10 @@ function getRandomInt(from, to) {
   const max = Math.floor(Math.max(from, to));
   return Math.floor(Math.random()*(max - min + 1) + min);
 }
-let numberInt = getRandomInt(0, 10);
+const numberInt = getRandomInt(0, 10);
 // Случайное число с плавающей точкой
 function getRandomFloat() {
   const randNumber = Math.random() * numberInt;
   return Math.floor(randNumber * 100) / 100;
 }
-
-
+getRandomFloat();
