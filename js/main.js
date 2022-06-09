55aac2c2d8d74a49b1305f561b24c81d7ba338f4
Наталия Лифанова <tymanblack@gmail.com>
2022-06-09 12:04:43 +0300

    Corrections made

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
-/**
-* Случайное целое число из положительного диапазона включительно, включая ноль (min; max).
-* Если значение "до" меньше или равно значению "от", то "Неверный диапазон".
-* Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-function getRandomInt(min, max) {
-  if (min < 0 || max <= 0 || max <= min) {
-    return ('Неверный диапазон');
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
+// Случайное целое число
+function getRandomInt(from, to) {
+  const min = Math.ceil(Math.min(from, to));
+  const max = Math.floor(Math.max(from, to));
   return Math.floor(Math.random()*(max - min + 1) + min);
 }
-getRandomInt(0, 50);
-/**
-* Случайное число с плавающей точкой из положительного диапазона включительно, включая ноль (min; max).
-* Если значение "до" меньше или равно значению "от", то "Неверный диапазон".
-* Источник toFixed: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-*/
-function getRandomFloat(min, max, number) {
-  if (min < 0 || max <= 0 || max <= min) {
-    return ('Неверный диапазон');
-  }
-  return (Math.random() * (max - min) + min).toFixed(number);
+let numberInt = getRandomInt(0, 10);
+// Случайное число с плавающей точкой
+function getRandomFloat() {
+  const randNumber = Math.random() * numberInt;
+  return Math.floor(randNumber * 100) / 100;
 }
-getRandomFloat(0, 20.9, 3);
+
+
